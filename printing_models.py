7e57ffb7ce45085fc15624f2defa0a0dbd750f2c
Coldroy <arthurlandahl@gmail.com>
2018-09-18 01:38:33 +0200

    Adding the last exercises from chapter8

---
+++ printing_models.py
@@ -0,0 +1,7 @@
+import print_models as pm
+#8.13 Printing models
+unprinted_designs=['Iphone8 Case','Bear Keyring','Android Plushy']
+completed_designs=[]
+pm.print_models(unprinted_designs,completed_designs)
+print(completed_designs)
+
